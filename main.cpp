ec937fdab331edba3bf83e48147dea65af0db18e
Thornary <151729561+Thornary@users.noreply.github.com>
2023-12-13 17:17:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,13 @@
-#include "NFCREADER.h"
-#include "NFCWRITER.h"
-#include "LED_PORTE.h"
-#include "Buzzer.h"
-#include "Servo_Porte.h"
+#include "Application.h"
 
-
-Classe_NFC_Lecture nfc;
-LED leds(D1,D2);
-Servo_Porte servo;
-Buzzer buzz(D8);
-
-byte CodeVerif=0; 
-byte CodeMauvais=0; 
-byte Code_Acces[4]={0xC3, 0x5A, 0x96, 0x4F}; 
-int presence;
+Application appli(D8,D1,D2);
 
 void setup() {
-Serial.begin(9600);
-nfc.init_nfc();
-leds.init();
-servo.init(D9);
-buzz.stop();
-
+appli.init();
 }
 
 
 void loop() {
-
-nfc.lecture_donnee_carte();
-delay(100);
-CodeVerif = nfc.GetAccesState(Code_Acces);
-CodeMauvais = nfc.MauvaisBadge();
-Serial.print(CodeVerif);
-
-if (CodeVerif==1 )
-{
-  
-  leds.clignoteBleuVert(6,100);
-  buzz.validationSound();
-  servo.activate();
-  delay(5000);
-  servo.deactivate();
-  nfc.stopAcces();
-}
-else if(CodeVerif==0 && CodeMauvais == 1) {
-  leds.clignoteRouge(6, 100);
-  buzz.refusalSound();
-  delay(1000);
-  nfc.stopAcces();
-}
-else{leds.rouge();}
+appli.run();
 
 }
